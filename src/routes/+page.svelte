707d2126186de8d04bc2c5dc4a7026557fd613fa
louquart <sweetlougq2@gmail.com>
2023-09-21 19:34:18 -0700

    routing template (client only)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+temp login/sign up
+<br/><br/><br/><br/>
+<div class="flex justify-center">
+    <h1 class="text-3xl font-bold">
+        <a href="/home">client login placeholder ✔️</a>
+    </h1>
+</div>
+<br/><br/><br/><br/>
+<div class="flex justify-center">
+    <h1 class="text-3xl font-bold">
+        <a href="/">PT login placeholder ❌</a>
+    </h1>
+</div>
